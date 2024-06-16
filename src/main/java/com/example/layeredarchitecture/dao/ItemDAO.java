19fa6c8870e7404b3eceade2eea261b5322a74d4
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2024-06-16 16:31:07 +0530

    Layered Architectured Project

---
+++ src/main/java/com/example/layeredarchitecture/dao/ItemDAO.java
@@ -0,0 +1,21 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.ItemDTO;
+import com.example.layeredarchitecture.model.OrderDetailDTO;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ItemDAO {
+    public ArrayList<ItemDTO> loadAllItems() throws SQLException, ClassNotFoundException;
+    public void deleteItem(String code) throws SQLException, ClassNotFoundException;
+    public void saveItems(String code, String description, BigDecimal unitPrice, int qtyOnHand) throws SQLException, ClassNotFoundException;
+    public void updateItem(String code, String description, BigDecimal unitPrice, int qtyOnHand) throws SQLException, ClassNotFoundException;
+    public boolean updateItems(List<OrderDetailDTO> orderDetails, ItemDTO item) throws SQLException, ClassNotFoundException;
+    public boolean exixtIds(String code) throws SQLException, ClassNotFoundException;
+    public String generateIds () throws SQLException, ClassNotFoundException;
+    public ItemDTO searchItems(String newItemCode) throws SQLException, ClassNotFoundException;
+}
